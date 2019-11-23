f283f08e988c319d8300ae36d86b7df2d2fb9616
kendrafarinas <47564990+kendrafarinas@users.noreply.github.com>
2019-11-23 10:36:53 -0400

    Add files via upload

---
+++ SI220Z1/Aritmetica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SI220Z1
+{
+    public class Aritmetica
+    {
+        public int sumar (int a, int b)
+        {
+            return a + b;
+        }
+        public int sumar(int a, object b)
+        {
+            if(b==null)
+            {
+                return a;
+            }     
+            else
+            {
+                return a + System.Convert.ToInt32(b); 
+            }
+        }
+    }
+}
---
+++ SI220Z1/Saludar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SI220Z1
+{
+    public class Saludar
+    {
+        public string DecirHola()
+        {
+            return "HOLA MUNDO";
+        }
+    }
+}
